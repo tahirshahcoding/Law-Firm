70678c8618656dff3a6a81cae06fc9b84c267ee1
EagleNest Legal <user@eaglenestlegal.pk>
2026-07-08 12:42:17 +0500

    Revert build.sh optimization

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-set -x
 
-echo "Starting collectstatic..."
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-echo "Starting migrate..."
-python manage.py migrate --no-input
-
-echo "Build complete."
+python manage.py migrate --no-input  
